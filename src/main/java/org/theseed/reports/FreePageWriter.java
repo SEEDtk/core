00b5bc357a2ea5711cb51b836ddeeb67a7f0d915
Bruce Parrello <bparrello@figresearch.com>
2021-04-19 14:29:51 -0500

    Fix to decouple FreePageWriter from STDOUT.

--- src/main/java/org/theseed/reports/FreePageWriter.java
+++ src/main/java/org/theseed/reports/FreePageWriter.java
@@ -5,6 +5,7 @@ package org.theseed.reports;
 
 import static j2html.TagCreator.*;
 
+import java.io.PrintWriter;
 import java.util.stream.Stream;
 
 import j2html.tags.ContainerTag;
@@ -20,19 +21,36 @@ import j2html.tags.EmptyTag;
  */
 public class FreePageWriter extends PageWriter {
 
+    // FIELDS
+    /** output stream */
+    private PrintWriter writer;
     /** style sheet link */
     public static final EmptyTag SEEDTK_STYLES = link().withRel("stylesheet").withHref("https://bioseed.mcs.anl.gov/~parrello/SEEDtk/css/Basic.css");
 
+    /** Construct a page writer for the system output */
+    public FreePageWriter() {
+        this.writer = new PrintWriter(System.out);
+    }
+
+    /**
+     * Construct a page writer for a specific output stream.
+     *
+     * @oaram writer		target output stream
+     */
+    public FreePageWriter(PrintWriter writer) {
+        this.writer = writer;
+    }
+
     @Override
     protected void writePage(String title, DomContent heading, Stream<DomContent> content) {
-        System.out.println(document().render());
+        this.writer.println(document().render());
         ContainerTag body = body().attr("onload", "setup();")
                 .with(h1(heading)).with(content);
         ContainerTag page = html(head(title(title), SEEDTK_STYLES,
                 script().withSrc("/SEEDtk/css/utils.js"), script().withSrc("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"),
                 link().withRel("icon").withType("image/png").withHref("https://bioseed.mcs.anl.gov/~parrello/SEEDtk/favicon.ico")), body);
-        System.out.println(page.render());
-        System.out.flush();
+        this.writer.println(page.render());
+        this.writer.flush();
     }
 
     @Override
